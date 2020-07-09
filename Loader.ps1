d858256b46eaeddc328836dcd7e17d0a5e6ecc86
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2020-07-09 19:13:35 -0400

    run main script in same window

--- Loader.ps1
+++ Loader.ps1
@@ -65,7 +65,5 @@ Start-Sleep -s 1
 #Unblocking all script files
 Write-Output "Unblocking files just in case"
 Get-ChildItem -Path $path\ParsecTemp -Recurse | Unblock-File
-Write-Output "Starting main script, this Window will close in 60 seconds"
-start-process powershell.exe -verb RunAS -argument "-file $path\parsectemp\PostInstall\PostInstall.ps1"
-Start-Sleep -Seconds 60
-stop-process -Id $PID
+Write-Output "Starting main script"
+.\PostInstall\PostInstall.ps1
